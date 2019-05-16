8e1ab1efb4498a4a84f3c3a548fe2220bfb9d41f
hendisantika <hendisantika@yahoo.co.id>
2019-05-17 04:36:13 +0700

    #3 - Add User Entity

---
+++ src/main/java/com/hendisantika/springbootmybatisdemo/entity/User.java
@@ -0,0 +1,36 @@
+package com.hendisantika.springbootmybatisdemo.entity;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mybatis-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-17
+ * Time: 04:35
+ */
+public class User {
+
+    private int id;
+
+    @NotBlank(message = "{require_check}")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
