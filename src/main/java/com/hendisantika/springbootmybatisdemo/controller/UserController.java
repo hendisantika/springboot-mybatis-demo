897c4a4a76315ccbfa2b56bd06e649e19e560b2a
hendisantika <hendisantika@yahoo.co.id>
2019-05-17 04:39:21 +0700

    #8 - Add getUser api in UserController

--- src/main/java/com/hendisantika/springbootmybatisdemo/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootmybatisdemo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -34,5 +35,12 @@ public class UserController {
         List<User> users = userMapper.selectAll();
         model.addAttribute("users", users);
         return "users/list";
+    }
+
+    @GetMapping("{id}")
+    public String getUser(@PathVariable int id, Model model) {
+        User user = userMapper.select(id);
+        model.addAttribute("user", user);
+        return "users/show";
     }
 }
